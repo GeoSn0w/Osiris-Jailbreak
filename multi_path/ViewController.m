7e214430c1f2b5747be3ba6c9b509c4739a30932
GeoSn0w <GeoSn0w@users.noreply.github.com>
2018-06-17 15:07:29 -0400

    Fixes

--- multi_path/ViewController.m
+++ multi_path/ViewController.m
@@ -9,44 +9,11 @@
 #import "ViewController.h"
 #include "sploit.h"
 #include "QiLin.h"
-#include "fuckDropbear.h"
 #include <dirent.h>
 #include <mach/mach.h>
 #include <objc/runtime.h>
 #import <Foundation/Foundation.h>
 
-@interface LSApplicationWorkspace : NSObject
-+ (id) defaultWorkspace;
-- (BOOL) registerApplication:(id)application;
-- (BOOL) unregisterApplication:(id)application;
-- (BOOL) invalidateIconCache:(id)bundle;
-- (BOOL) registerApplicationDictionary:(id)application;
-- (BOOL) installApplication:(id)application withOptions:(id)options;
-- (BOOL) _LSPrivateRebuildApplicationDatabasesForSystemApps:(BOOL)system internal:(BOOL)internal user:(BOOL)user;
-@end
-
-Class lsApplicationWorkspace = NULL;
-LSApplicationWorkspace* workspace = NULL;
-
-void uicache(void) {
-    
-    
-    if(lsApplicationWorkspace == NULL || workspace == NULL) {
-        lsApplicationWorkspace = (objc_getClass("LSApplicationWorkspace"));
-        workspace = [lsApplicationWorkspace performSelector:@selector(defaultWorkspace)];
-    }
-    
-    if ([workspace respondsToSelector:@selector(_LSPrivateRebuildApplicationDatabasesForSystemApps:internal:user:)]) {
-        if (![workspace _LSPrivateRebuildApplicationDatabasesForSystemApps:YES internal:YES user:NO])
-            printf("[ERROR]: failed to rebuild application databases\n");
-        
-    }
-    
-    if ([workspace respondsToSelector:@selector(invalidateIconCache:)]) {
-        [workspace invalidateIconCache:nil];
-    }
-    
-}
 @interface ViewController ()
 
 @end
@@ -61,9 +28,27 @@ void uicache(void) {
 }
 
 - (IBAction)jbMeNow:(UIButton *)sender {
-    self.jailbreakMeNowBtn.enabled = NO;
-   [self.jailbreakMeNowBtn setTitle:@"Running the Exploit" forState:UIControlStateDisabled];
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 11.3) {
+        UIAlertController * failure_alert = [UIAlertController
+                                             alertControllerWithTitle:@"You are running iOS 11.3.x!"
+                                             message:@"The remount doesn't currently work on iOS 11.3.x! Working on it!"
+                                             preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction* cancel = [UIAlertAction
+                                 actionWithTitle:@"Dismiss"
+                                 style:UIAlertActionStyleDefault
+                                 handler:^(UIAlertAction * action) {
+                                     exit(EXIT_FAILURE);
+                                 }];
+        
+         [failure_alert addAction:cancel];
+         [self presentViewController:failure_alert animated:YES completion:nil];
+         [self failure];
+    } else {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+           self.jailbreakMeNowBtn.enabled = NO;
+           [self.jailbreakMeNowBtn setTitle:@"Running the Exploit" forState:UIControlStateDisabled];
+           dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
             
             if (exploit() != 0){
                 [self failure];
@@ -72,6 +57,9 @@ void uicache(void) {
             }
             
     });
+        });
+    }
+   
 }
 - (void) get_tfp0{
     unsigned int tfp0 = tfp0_printout();
